eb2517590f32f0364f80169e194c6effd6e4525e
Nourah Alsaadan <84617366+nourahsaud@users.noreply.github.com>
2022-05-31 09:25:43 +0300

    Added the lab files
    - didn't solve the cookies nether the sessions

---
+++ Good_Reads/books/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+#add namespace for the app
+app_name = "book"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("add/", views.add_book, name="add_book"),
+    path("detail/<book_id>", views.book_detail, name="book_details"),
+]
