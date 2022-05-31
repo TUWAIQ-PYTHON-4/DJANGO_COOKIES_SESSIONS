20fefc8f012bc25eea21a577412f3b3f044f431f
bushra <bushra.azbm@gmail.com>
2022-05-31 13:10:34 +0300

    modifed on cookies and session

--- Books/booksapp/urls.py
+++ Books/booksapp/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 app_name= 'books'
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("add/", views.add_book, name="add_book"),
+    path("add_book/", views.add_book, name="add_book"),
     path("detail/<book_id>", views.book_detail, name="book_detail"),
+    path("change_size/", views.change_size, name="change_size"),
+    path("fav_book/<book_id>", views.fav_book, name="fav_book")
 ]
--- Books/booksapp/views.py
+++ Books/booksapp/views.py
@@ -3,32 +3,45 @@ from .models import Book,Comment
 from .forms import *
 # Create your views here.
 
+
+def fav_book(request, book_id):
+    fav_book = Book.objects.get(pk=book_id)
+    save_book = request.session['fav_books']
+
+    if fav_book not in request.session['fav_books']:
+        request.session['fav_books'] = [book_id]
+        list_saved= Book.objects.get(id=book_id)
+        save_book.append(list_saved)
+
+    response = render(request, 'books/fav_book.html', {'fav_book': save_book})
+    return response
+
+
+def change_size(request):
+    font_size = request.GET.get('font_size')
+    if font_size == 'big':
+        font_size = '40px'
+    elif font_size == 'small':
+        font_size == '16px'
+    else:
+        font_size == '23px'
+
+    response = render(request, 'books/index.html', {'font_size': font_size})
+    response.set_signed_cookie("font_size", 'font_size')
+    return response
+
 def index(request):
 
     book_list= Book.objects.all()
-
-    response= render(request, 'books/index.html', {'books':book_list})
-
-    request.session["fav_books"] = ["Harry Potter", "Secret Garden"]
-
-    signed_cookie = request.get_signed_cookie("important", None)
-
-    if "fontsize" in request.GET:
-        current_font = request.COOKIES['fontsize']
-        print("GET:fontsize" +request.COOKIES.get('fontsize'))
-
-    elif "fontsize" in request.POST:
-        current_font = request.POST.get('fontsize')
-        print("POST:fontsize" +request.POST['fontsize'])
-
-    response.set_signed_cookie("fontsize", 'current_font')
+    font_size = request.COOKIES.get('font_size', '16px')
+    response= render(request, 'books/index.html', {'books':book_list, 'font_size': font_size})
 
     return response
 
 def add_book(request):
 
     if request.method == 'POST':
-        bookform = BookForm(request.POST, request.Files)
+        bookform = BookForm(request.POST)
 
         if bookform.is_valid():
            book = bookform.save()
