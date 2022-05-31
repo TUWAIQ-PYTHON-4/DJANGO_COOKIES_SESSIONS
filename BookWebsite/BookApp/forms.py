07f4013bc3c65e1aecbc48e4833306f1fdfb2564
t h <taif@gmail.com>
2022-05-31 08:36:42 +0300

    solved lab

---
+++ BookWebsite/BookApp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+class CommentForm(forms.Form):
+
+    name = forms.CharField(max_length=124)
+    content = forms.CharField(widget=forms.widgets.Textarea)
---
+++ BookWebsite/BookApp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=300)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+     book = models.ForeignKey(Book, on_delete=models.CASCADE)
+     name = models.CharField(max_length=124)
+     content = models.TextField()
+     date = models.DateTimeField(auto_now_add=True)
