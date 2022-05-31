412aa4e65318bd11f26e83c4e7c8278d38159b4b
lama <l4m4100@gmail.com>
2022-05-31 21:31:11 +0300

    update

--- book/book1/urls.py
+++ book/book1/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("list/<book_index>", views.list_books, name="list_movies"),
     path("add/", views.add_books, name="add_book"),
     path("detail/<book_id>", views.book_detail, name="book_detail"),
+    path("add_favorite/<book_id>", views.add_favorite, name="add_favorite")
+
 ]
--- book/book1/views.py
+++ book/book1/views.py
@@ -1,8 +1,15 @@
+from urllib import response
+
 from django.shortcuts import render , resolve_url , redirect
 from django.http import HttpRequest
 from .forms import bookModelForm, CommentForm
 from .models import Comment, book
 
+
+def add_favorite(request:HttpRequest, book_id):
+    request.session["favs"] = request.session.get("favs", [])+ [book_id]
+    print(request.session["favs"])
+    return redirect(resolve_url("books:index"))
 
 def index(request: HttpRequest):
     book_list = book.objects.all()
@@ -10,14 +17,14 @@ def index(request: HttpRequest):
     context = {"books": book_list, "display": True}
     response = render(request, 'index.html', context)
 
+    if 'font_size' in request.GET:
+        response.set_cookie('font_size', request.GET['font_size'])
 
-#    request.session["fav_books"] = ["book1", "book2"]
-
-
-    signed_cookie = request.get_signed_cookie("important", None)
-
-    if "chang_size" in request.GET:
-        response.set_signed_cookie("important", "some value")
+    if 'favs_only' in request.GET:
+        favorite_book = request.session.get("favs", [])
+        movie_list = book.objects.filter(id__in=favorite_book)
+    else:
+        movie_list = book.objects.all()
 
     return response
 
@@ -25,9 +32,6 @@ def index(request: HttpRequest):
 def book_browse(request:HttpRequest, book_id):
     Book = book.objects.get(pk=book_id)
 
-    #get the session data or none
-    session_content = request.session.get("fav_movies", None)
-    print(session_content)
 
     if request.method == "POST":
         comment_form = CommentForm(request.POST)
@@ -42,6 +46,11 @@ def book_browse(request:HttpRequest, book_id):
 
     return render(request, 'book_detail.html', context)
 
+def cookies(request):
+    signed_cookie = request.get_signed_cookie("important", None)
+
+    if "chang_size" in request.GET:
+        response.set_signed_cookie("important", "some value")
 
 def list_books(request : HttpRequest, book_index):
 
@@ -50,7 +59,7 @@ def list_books(request : HttpRequest, book_index):
 
     books = ["bbok1", "book2", "book3"]
     print(book_index)
-    context = {"movie" : books[int(book_index)]}
+    context = {"book" : books[int(book_index)]}
     return render(request, 'list.html', context)
 
 
@@ -61,17 +70,13 @@ def add_books(request : HttpRequest):
 
         if bookModelForm.is_valid():
             book = booksModelForm.save()
-            return redirect(resolve_url("book:index"))
+            return redirect(resolve_url("books:index"))
 
     form = bookModelForm()
     return render(request, 'add_book.html', {"form" : form})
 
 def book_detail(request:HttpRequest, book_id):
     books = book.objects.get(pk=book_id)
-
-    #get the session data or none
-    session_content = request.session.get("fav_books", None)
-    print(session_content)
 
     if request.method == "POST":
         comment_form = CommentForm(request.POST)
@@ -82,6 +87,6 @@ def book_detail(request:HttpRequest, book_id):
             print(comment_form.errors)
 
 
-    context = {"book" : book, "form" : CommentForm()}
+    context = {"Comment" : Comment, "form" : CommentForm()}
 
     return render(request, 'book_detail.html', context)
